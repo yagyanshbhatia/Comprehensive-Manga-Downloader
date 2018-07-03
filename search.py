f1b3c537e2256ecec967abc5622a023a80ad6862
yagyanshbhatia <yagyansh1999@gmail.com>
2018-07-03 11:35:28 +0530

    everything finally integrated, working

--- search.py
+++ search.py
@@ -1,13 +1,27 @@
 from selenium import webdriver
-# import time
+import os
+import time
+import zipfile
+from fpdf import FPDF
 
 chromedriver = "/home/yagyansh/Desktop/chromedriver_linux64/chromedriver"
-driver = webdriver.Chrome(chromedriver)
+chrome_options = webdriver.ChromeOptions()
+prefs = {'download.default_directory' : os.getcwd()}
+chrome_options.add_experimental_option('prefs', prefs)
+driver = webdriver.Chrome(chromedriver, chrome_options=chrome_options)
 
-driver.get("http://www8.mangafreak.net/")
 name = input("Enter the name of Anime to search - ").lower()
 name = name.split()
 driver.get("http://www8.mangafreak.net/Search/" + '%20'.join(name))
+
+while (1):
+    try:
+        done = driver.find_element_by_link_text("Home")
+        break
+    except Exception as e:
+        time.sleep(1)
+        continue
+
 result = driver.find_elements_by_class_name("manga_search_item")
 for r in result:
     print(r.text)
@@ -18,14 +32,6 @@ interest = result[index]
 link = interest.find_element_by_tag_name("a")
 link.click()
 
-# I have to wait now untill page loads. this can be done by
-# while (1):
-#     try:
-#         done = driver.find_element_by_link_text("Home")
-#         break
-#     except Exception as e:
-#         time.sleep(1)
-#         continue
 
 lists = driver.find_element_by_class_name("manga_series_list")
 rows = lists.find_elements_by_tag_name("tr")
@@ -43,5 +49,55 @@ else:
     end = int(input("end = "))
     for i in range(start, end + 1):
         interest = rows[i]
-        print("Now Downloading ", interest.find_elements_by_tag_name("td")[0].text)
         down = interest.find_element_by_link_text("Download").click()
+        print("Now Downloading ", interest.find_elements_by_tag_name("td")[0].text)
+
+time.sleep(1)
+
+driver.get('chrome://downloads')
+while True:
+    for item in driver.find_elements_by_css_selector('body/deep/downloads-item'):
+        if 'pause' in item.text.lower():
+            time.sleep(2)
+            break
+    else:
+        break
+
+def natural_keys(text):
+    c = text.split(".")[0].split('_')[1]
+    return int(c)
+
+listFiles = []
+cwd = os.getcwd()
+for file in os.listdir(cwd):
+    if file.endswith(".zip"):
+        listFiles.insert(1, str(file))
+
+listFiles.sort(key=natural_keys)
+for file in listFiles:
+    path = file.split('.')[0]
+    try:
+        os.mkdir(path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        try:
+            zip_ref = zipfile.ZipFile(os.path.join(cwd, file), 'r')
+            zip_ref.extractall(path)
+            zip_ref.close()
+
+            mypath = os.path.join(cwd,path)
+            onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+
+            def natural_keys_1(text):
+                c = text.split(".")[0].split('_')[-1]
+                return int(c)
+
+            onlyfiles.sort(key=natural_keys_1)
+            pdf = FPDF()
+            for image in onlyfiles:
+                pdf.add_page()
+                pdf.image(mypath + '/' + image, 0, 0, w=210, h=297)
+            pdf.output(file.split('.')[0] + ".pdf", "F")
+        except:
+            continue
